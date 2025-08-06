dc3eb2aa1232dca22794eb2308e9c5d88a195e35
dbossaller <168793316+dbossaller@users.noreply.github.com>
2025-08-06 15:02:41 -0500

    Given subject number, script to find all dataframes when they are walking.

--- find_walking_frames.py
+++ find_walking_frames.py
@@ -5,23 +5,48 @@ import pandas as pd
 import numpy as np
 def main():
     directory = 'data/'
-    test_subject_num = '10'
-    files_with_walk(test_subject_num, directory)
+    test_subject_num = '2'
+    print(find_walking_data(test_subject_num, directory))
 
-def files_with_walk(test_subject_num, directory):
+def find_walking_data(test_subject_num, directory):
+    test_subject_num = str(test_subject_num)
+    walking_dict = {}
     test_subject_num = test_subject_num.zfill(2)
     for filename in os.listdir(directory):
-        pattern = f'{test_subject_num}_.+txt$'
+        pattern = rf'_various_{test_subject_num}_.+txt$'
         if re.search(pattern, filename):
-            with open(directory+filename) as f:
-                    # This won't return all filenames, only the first.
-                    return walking_data_frames(directory,filename)           
+            if walking_dataframes(directory,filename):
+                walking_dict[filename[-9:-4]] = walking_dataframes(directory, filename)
+            else:
+                continue
         else:
             continue
+    return walking_dict
 
         
-def walking_data_frames(directory, filename):
-    pass
+def walking_dataframes(directory, filename):
+    df = pd.read_csv(directory+filename, sep = '\t', header = 3)
+    with open(directory+filename, 'r') as file:
+        firstline = file.readlines()[0]
+        
+    line1 = firstline.lstrip('#Activity\t').rstrip(' \n').split(' ')
+    line1 = line1[0:-2]
+    if 'walking' not in line1:
+        return False
+    else:
+        walking_idx = df.loc[df['act'] == 1].index
 
+        walking_idx_starts = [walking_idx[0]]
+        walking_idx_ends = []
+        for i in range(1, len(walking_idx)):
+            if walking_idx[i] != walking_idx[i-1] + 1:
+                walking_idx_starts.append(walking_idx[i])
+                walking_idx_ends.append(walking_idx[i-1])
+        walking_intervals = [(s,e) for (s,e) in zip(walking_idx_starts, walking_idx_ends)]
+        walk_dfs = []
+        for int in walking_intervals:
+            walk_dfs.append(df.iloc[int[0]:int[1]])
+
+    return walk_dfs
 if __name__ == '__main__':
     main()
