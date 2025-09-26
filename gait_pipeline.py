28b6f815c2420ab3748ef6fc706bd78edadaadff
dbossaller <168793316+dbossaller@users.noreply.github.com>
2025-09-26 16:14:26 -0500

    Function to make entropy dataframe.

--- gait_pipeline.py
+++ gait_pipeline.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
 from gtda.diagrams import PersistenceEntropy, Scaler
 from gtda.homology import VietorisRipsPersistence
 from gtda.pipeline import Pipeline
@@ -20,3 +24,21 @@ def topological_transform(signals):
     H0_H1_H2_entropies = topological_transformer.fit_transform(signals)
     
     return H0_H1_H2_entropies
+
+def calculate_entropies(full_dataset):
+    entropies = []
+    for subject in full_dataset.keys():
+        start_time = datetime.now()
+        print(f'Calculating entropies for subject {subject}')
+        for measurement in full_dataset[subject]:
+            measurement = np.array(measurement)[None,:,:]
+            H0, H1, H2 = topological_transform(measurement)[0]
+            entropies.append([subject, H0, H1, H2])
+        print(f'\tCompleted, took {(datetime.now()-start_time).total_seconds():.0f} seconds')
+    return entropies
+
+def make_entropy_dataframe(entropies):
+    return pd.DataFrame(entropies, columns = ['subject', 'H0 Entropy', 'H1 Entropy', 'H2 Entropy'])
+
+def entropy_dataframe(full_dataset):
+    return make_entropy_dataframe(calculate_entropies(full_dataset))
